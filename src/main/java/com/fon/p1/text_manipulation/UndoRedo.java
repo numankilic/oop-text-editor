754529e5bdf09e1ec02a00f8aff6f230edc15282
numankilic <numankilic35@gmail.com>
2020-12-14 17:24:13 +0300

    UndoRedo class duzeltildi

---
+++ src/main/java/com/fon/p1/text_manipulation/UndoRedo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fon.p1.text_manipulation;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author Numan
+ */
+public class UndoRedo {
+    Stack<String> undoStack = new Stack<>();
+    Stack<String> redoStack = new Stack<>();
+    
+    public UndoRedo() {
+        
+    }
+    
+    public void toStack(String writtenChar){
+        System.out.println("Stack'e atilan char: " + writtenChar);
+        undoStack.push(writtenChar);
+        for (int i = 0; i < undoStack.size();i++){
+            String ele = undoStack.pop();
+            System.out.println("Stackten cikan char: " + ele);
+        }
+    }
+    
+}
